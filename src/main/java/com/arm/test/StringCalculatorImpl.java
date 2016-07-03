3991c33779f80080f5c2227832c6d22872fa8b50
Pradeep <pradeep.sadashivamurthy@thomsonreuters.com>
2016-07-03 23:22:31 +0100

    Removed unwanted fields

--- src/main/java/com/arm/test/StringCalculatorImpl.java
+++ src/main/java/com/arm/test/StringCalculatorImpl.java
@@ -1,6 +1,5 @@
 package com.arm.test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -14,10 +13,6 @@ public class StringCalculatorImpl implements StringCalculator {
     private static Pattern pattern = Pattern.compile(DELIMITER);
 
     private static final int BIG_NUMBER_LIMIT = 1000;
-
-    private static final int ZERO = 0;
-
-    private List<Integer> negativeNumbers = new ArrayList<>();
 
     private CharactersFilter charactersFilter;
 

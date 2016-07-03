0ad861d917bd766d3ba617a1ea334f1e1eaab3bb
Pradeep <pradeep.sadashivamurthy@thomsonreuters.com>
2016-07-03 20:45:05 +0100

    Added a test to allown unwknown amount of numbers

--- src/test/java/com/arm/test/StringCalculatorTest.java
+++ src/test/java/com/arm/test/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.arm.test;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -13,6 +14,20 @@ public class StringCalculatorTest {
     private StringCalculator stringCalculator = new StringCalculatorImpl();
 
     @Test
+    public void shouldAddForAnEmptyString() {
+        int result = stringCalculator.add("");
+
+        assertThat(result, is(0));
+    }
+
+    @Test
+    public void shouldAddForStringValueOne() {
+        int result = stringCalculator.add("1");
+
+        assertThat(result, is(1));
+    }
+
+    @Test
     public void shouldAddFromAGivenString() {
         int sum = stringCalculator.add("2,3");
 
@@ -20,9 +35,9 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void shouldAddForAnEmptyString() {
-        int result = stringCalculator.add("");
+    public void testAddToAllowUnknownAmountOfNumbers() {
+        int result = stringCalculator.add("123456789,1,10,100");
 
-        assertThat(result, is(0));
+        assertThat(result, CoreMatchers.is(123456900));
     }
 }

4374547e4d3c75fe415f46322639471197c23d41
Pradeep <pradeep.sadashivamurthy@thomsonreuters.com>
2016-07-03 23:17:00 +0100

    Fixed test case logNegativeNumberAndThrowException

--- src/main/java/com/arm/test/NumbersFilter.java
+++ src/main/java/com/arm/test/NumbersFilter.java
@@ -5,9 +5,11 @@ import java.util.List;
 /**
  * Number filter filters or handles number related operations
  */
-public interface NumberFilter {
+public interface NumbersFilter {
 
     List<Integer> filterBigNumbers(List<Integer> listOfNumbers, Integer limit);
+
+    List<Integer> logNegativeNumberAndThrowException(List<Integer> listOfNumbers);
 
     List<Integer> mapToInt(List<String> listOfString);
 }
---
+++ src/main/java/com/arm/test/NumbersFilterImpl.java
@@ -0,0 +1,44 @@
+package com.arm.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Default implementation for {@code NumbersFilter}
+ */
+public class NumbersFilterImpl implements NumbersFilter {
+
+    public static final String NEGATIVES_NOT_ALLOWED = "Negatives not allowed ";
+
+    @Override
+    public List<Integer> filterBigNumbers(List<Integer> listOfNumbers, Integer limit) {
+        return listOfNumbers.stream().filter(seq -> seq < limit).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Integer> logNegativeNumberAndThrowException(List<Integer> listOfNumbers) {
+        List<Integer> integers = new ArrayList<>();
+
+        listOfNumbers = listOfNumbers.stream().filter(seq -> {
+            if(seq < 0)
+                integers.add(seq);
+
+            return true;
+        }).collect(Collectors.toList());
+
+        if(integers.size() > 0)
+            throw new IllegalArgumentException(NEGATIVES_NOT_ALLOWED + integers.toString());
+
+        return listOfNumbers;
+    }
+
+    @Override
+    public List<Integer> mapToInt(List<String> listOfString) {
+        return listOfString
+                .stream()
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
